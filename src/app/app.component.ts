6101192f0c4887d185347ad36c9aaa393929fb09
Technous Dev2 <technousdev2@Technouss-MacBook-Pro.local>
2023-08-11 22:19:55 +0530

    chnages done according to the the new app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
-import { ProductService } from './products/product.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -13,7 +12,7 @@ export class AppComponent implements OnInit , DoCheck{
   // clickmeactive:boolean =false;
   // subscription:Subscription;
   isMenuTabsRequired:boolean  
-  constructor(public product : ProductService , private route :Router){
+  constructor( private route :Router){
   }
   ngOnInit(): void {
 
@@ -28,14 +27,4 @@ export class AppComponent implements OnInit , DoCheck{
       this.isMenuTabsRequired = true
     }
   }
-  // ngAfterViewInit(): void {
-  //  this.subscription= this.product.productedAddded.subscribe(data=>{
-  //     console.log("===" , data)
-  //     this.clickmeactive = data
-  //   })
-  // }
-
-  // ngOnDestroy(): void {
-  //   this.subscription.unsubscribe();
-  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Product1sModule } from './product1s/product1s.module';
-import { TestingModule } from './testing/testing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatrialModule } from 'src/matrial.module';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -13,8 +11,6 @@ import { ToastrModule } from 'ngx-toastr';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
-import { UserLisrComponent } from './user-lisr/user-lisr.component';
-import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +18,10 @@ import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    UserLisrComponent,
-    UpdatepopupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TestingModule,
     BrowserAnimationsModule,
     MatrialModule,
     ReactiveFormsModule,
